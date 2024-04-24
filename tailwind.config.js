dc7be2bc9b87b0cfea54117db0b03127b135a9fd
kendpr <real.kendpr@gmail.com>
2024-04-24 15:37:24 +0800

    Styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        "text-sm": "",
+        text2xl: "",
+      },
+      colors: {
+        neutral: {
+          100: "hsl(0, 0%, 100%)",
+          200: "hsl(212, 45%, 89%)",
+          500: "hsl(220, 15%, 55%)",
+          900: "hsl(218, 44%, 22%)",
+        },
+      },
+      fontFamily: {
+        display: ["Outfit", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
